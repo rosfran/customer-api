5963884dadabfe3721bd1b622e6f4007d2dec6fd
Rosfran Lins Borges <rosfran.borges@cnj.jus.br>
2024-10-23 21:18:23 +0200

    refact: chores.

--- src/main/java/com/hr/management/service/AuthService.java
+++ src/main/java/com/hr/management/service/AuthService.java
@@ -22,7 +22,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
